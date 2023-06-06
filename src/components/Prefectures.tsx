3d62426f68f11de09446e80e9746a1bff729f141
Jaehee Chung <brillar2225@gmail.com>
2023-06-06 18:17:45 +0900

    STYLE: 選択ボタンに関するCSSを追加

--- src/components/Prefectures.tsx
+++ src/components/Prefectures.tsx
@@ -14,20 +14,22 @@ export default function Prefectures({ chartData, prefDatas, handleClick, handleC
 			<h1 className="sr-only">以下より気になる都道府県を選択して下さい</h1>
 			<div className="aside__wrapper">
 				<h1 className="aside__title">都道府県</h1>
-				<div className="aside__btns">
-					<h2>ご希望の情報を選択して下さい（デフォルトは総人口です）</h2>
-					<button type="button" name="総人口" onClick={handleClick}>
-						総人口
-					</button>
-					<button type="button" name="少年人口" onClick={handleClick}>
-						少年人口
-					</button>
-					<button type="button" name="生産年齢人口" onClick={handleClick}>
-						生産年齢人口
-					</button>
-					<button type="button" name="老人人口" onClick={handleClick}>
-						老人人口
-					</button>
+				<div className="aside__btn-container">
+					<h2 className="aside__sub-title">ご希望の情報を選択して下さい</h2>
+					<div className="aside__btns">
+						<button type="button" className="aside__btn" name="総人口" onClick={handleClick}>
+							総人口&#40;DEFAULT&#41;
+						</button>
+						<button type="button" className="aside__btn" name="少年人口" onClick={handleClick}>
+							少年人口
+						</button>
+						<button type="button" className="aside__btn" name="生産年齢人口" onClick={handleClick}>
+							生産年齢人口
+						</button>
+						<button type="button" className="aside__btn" name="老人人口" onClick={handleClick}>
+							老人人口
+						</button>
+					</div>
 				</div>
 				<ul className="aside__checkbox-list">
 					{prefDatas?.result.map((info) => (
